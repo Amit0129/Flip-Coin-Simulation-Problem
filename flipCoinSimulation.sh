037366402d24fee8b58c4d502d8b241e0800ea91
Amit0129 <amitkumarnayak40@gmail.com>
2022-09-26 18:21:26 +0530

    Added: UC2As a simulator, loop through Multiple times of flipping a coin and show number of times head and tail has won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,7 +1,26 @@
-toss=$(( RANDOM % 2 ))
-if [[ $toss == 0 ]]
+read -p "Enter no of times to toss: " n
+heads_count=0
+tails_count=0
+for[[ i = 1; i <= n; i++ ]]
+do
+    toss=$(( RANDOM % 2 ))
+    if[[ toss == 0 ))
+    then
+        echo "Heads"
+        (( heads_count++ ))
+    else
+        echo "Tails"
+        (( tails_count++ ))
+    fi
+done
+echo "The Heads count is $heads_count"
+echo "Tails Count is $tails_count"
+if[[ heads_count > tails_count ]]
 then
-    echo "Heads"
+    echo "Winner is Heads"
+elif[[ tails_count > heads_count ]]
+then
+    echo "Winner is Tails"
 else
-    echo "Tails"
+    echo "It is tie"
 fi
